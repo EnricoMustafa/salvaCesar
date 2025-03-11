2aa7b4e17d16a0e56e8b3abfd918e28beb1678b8
enrico brasil <enrico.brasil@prit.app>
2025-03-11 17:27:15 -0300

    final

--- automacao.py
+++ automacao.py
@@ -3,29 +3,31 @@ import time
 
 def aut(vezes): 
     for i in range(vezes):
-        pa.click(x=535, y=740);
+        pa.click(x=563, y=736);
         pa.click();
         pa.moveTo(x=646, y=628);
-        pa.scroll(-450);
-        pa.click(x=428, y=669);
-        pa.click(x=940, y=792);
+        pa.scroll(-490);
+        time.sleep(2);
+        pa.scroll(-100);
+        pa.click(x=417, y=623);
+        pa.click(x=942, y=789);
         time.sleep(6)
         pa.scroll(-200);
-        pa.click(x=1596, y=855);
-        pa.click(x=800, y=432);
-        pa.click(x=1531, y=875);
-        pa.click(x=469, y=306);
+        pa.click(x=1571, y=856);
+        pa.click(x=786, y=438);
+        pa.click(x=1447, y=875);
+        pa.click(x=452, y=303);
         time.sleep(3)
         pa.write('239');
-        pa.click(x=526, y=300);
+        pa.click(x=527, y=303);
         time.sleep(2)
-        pa.click(x=953, y=887);
+        pa.click(x=937, y=887);
         time.sleep(2)
         pa.moveTo(x=1644, y=664);
         pa.scroll(-600);
-        pa.click(x=882, y=983);
+        pa.click(x=872, y=979);
         time.sleep(2)
-        pa.click(x=1172, y=605);
+        pa.click(x=1128, y=602);
         time.sleep(6)
 
         print(f'Execução número {i+1}');
